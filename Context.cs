8d0d0fb298c9f3562f9bbe633968009accea6650
Hamit CANPOLAT <hhc_@hotmail.com.tr>
2023-03-02 23:54:32 +0300

    ErpS ilk gönderim

---
+++ Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpSinav.Entity
+{
+    public class Context : DbContext
+    {
+        public Context() : base("connection") { }
+        public DbSet<Kategori> kategoris { get; set; }
+        public DbSet<Urun> uruns{ get; set; }
+        public DbSet<Satis> Satis { get; set; }
+       
+    }
+}
---
+++ Entity/Satis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpSinav.Entity
+{
+    public class Satis
+    {
+
+        public int SatisId { get; set; }
+        public int SatisAdeti { get; set; }
+        public decimal ToplamFiyat { get; set; }
+
+        public int UrunId { get; set; }
+        public Urun Urun { get; set; }
+
+    }
+}
